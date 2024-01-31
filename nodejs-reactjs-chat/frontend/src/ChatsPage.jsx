c778d1ccdd8ca319e763c63536638f32656bef16
Gayathri P <132088009+GayathriPCh@users.noreply.github.com>
2024-01-31 15:45:11 +0000

    Base for a chat app, but without authentication.

---
+++ nodejs-reactjs-chat/frontend/src/ChatsPage.jsx
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId='b90b099f-6888-4cdd-8371-f1be251beafd'
+        username={props.user.username} // adam
+        secret={props.user.secret} // pass1234
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
