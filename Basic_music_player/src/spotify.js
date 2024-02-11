7b1185d01e91316988b05b5e6df39e66c0067de5
Gayathri P <132088009+GayathriPCh@users.noreply.github.com>
2024-02-11 15:56:21 +0530

    Update spotify.js

--- Basic_music_player/src/spotify.js
+++ Basic_music_player/src/spotify.js
@@ -13,11 +13,45 @@ const apiClient = axios.create({
   baseURL: "https://api.spotify.com/v1/",
 });
 
+let accessToken = ""; // Variable to store the current access token
+
 export const setClientToken = (token) => {
+  accessToken = token;
+
   apiClient.interceptors.request.use(async function (config) {
-    config.headers.Authorization = "Bearer " + token;
+    config.headers.Authorization = "Bearer " + accessToken;
     return config;
   });
+};
+
+export const getAccessToken = () => {
+  return accessToken;
+};
+
+export const refreshAccessToken = async (refreshToken) => {
+  try {
+    const response = await axios.post(
+      "https://accounts.spotify.com/api/token",
+      `grant_type=refresh_token&refresh_token=${refreshToken}&client_id=${clientId}`,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+
+    if (response.status === 200) {
+      const newAccessToken = response.data.access_token;
+      setClientToken(newAccessToken);
+      return newAccessToken;
+    } else {
+      console.error("Error refreshing access token:", response);
+      return null;
+    }
+  } catch (error) {
+    console.error("Error refreshing access token:", error);
+    return null;
+  }
 };
 
 export default apiClient;
