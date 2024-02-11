891c82cc8e90221e82f0c9bfd5a68c262b9d511c
Gayathri P <132088009+GayathriPCh@users.noreply.github.com>
2024-02-11 17:46:13 +0530

    refresh token thing didnt work

--- Basic_music_player/src/spotify.js
+++ Basic_music_player/src/spotify.js
@@ -1,4 +1,5 @@
-import axios from "axios"; 
+User
+import axios from "axios";
 
 const authEndpoint = "https://accounts.spotify.com/authorize?";
 const clientId = "573301f29ccc4880bb3ca1d0b48aff96";
@@ -13,45 +14,11 @@ const apiClient = axios.create({
   baseURL: "https://api.spotify.com/v1/",
 });
 
-let accessToken = ""; // Variable to store the current access token
-
 export const setClientToken = (token) => {
-  accessToken = token;
-
   apiClient.interceptors.request.use(async function (config) {
-    config.headers.Authorization = "Bearer " + accessToken;
+    config.headers.Authorization = "Bearer " + token;
     return config;
   });
 };
 
-export const getAccessToken = () => {
-  return accessToken;
-};
-
-export const refreshAccessToken = async (refreshToken) => {
-  try {
-    const response = await axios.post(
-      "https://accounts.spotify.com/api/token",
-      `grant_type=refresh_token&refresh_token=${refreshToken}&client_id=${clientId}`,
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      }
-    );
-
-    if (response.status === 200) {
-      const newAccessToken = response.data.access_token;
-      setClientToken(newAccessToken);
-      return newAccessToken;
-    } else {
-      console.error("Error refreshing access token:", response);
-      return null;
-    }
-  } catch (error) {
-    console.error("Error refreshing access token:", error);
-    return null;
-  }
-};
-
-export default apiClient;
+export default apiClient; 
